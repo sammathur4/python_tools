59cecc937e82aaa148df1405b3a4bc2451936d27
Saksham Mathur <60032069+sammathur4@users.noreply.github.com>
2024-02-05 22:26:12 +0530

    Update using_sdk.py

--- etc/using_sdk.py
+++ etc/using_sdk.py
@@ -1,7 +1,5 @@
 import os
 from notion_client import Client
-NOTION_TOKEN = "secret_BRFpq7gEYhabxTWGsZLBR2D8XhPe5ybcnsWbpNlmfmK"
-notion = Client(auth="secret_BRFpq7gEYhabxTWGsZLBR2D8XhPe5ybcnsWbpNlmfmK")
 
 
 from pprint import pprint
